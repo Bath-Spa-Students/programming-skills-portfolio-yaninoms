a48d3ae3f1e997d76c84b7f96cd507f0611393b7
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-01 13:12:30 +0400

    chap1exercise5.py

---
+++ Chapter 1- Getting Started with Python/exercise 5.py
@@ -0,0 +1,12 @@
+## Exercise 5: Compute area of Circle
+
+#Input the radius
+print("Area or Circumference of a Circle")
+PI=3.14
+radius= float(input('Input the radius of a cirlce:'))
+
+#Computation
+area = PI*radius*radius
+
+#Result
+print("The area is %.2f" %area)
---
+++ Chapter 2- Variables & Comments/Exercises/exercise 1.py
@@ -0,0 +1,15 @@
+# Exercise 1: Variables
+
+# Assign a message to a variable, and print that message.
+a="Bath Spa"
+b=" University"
+c=a+b
+print (c)
+
+# Then change the value of the variable to a new message, and print the newmessage.
+a="New York"
+c=a+b
+print (c)
+
+
+
