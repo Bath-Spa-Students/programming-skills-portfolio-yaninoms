36eddd48e940c0eb52eac1bea8c473ce24e6746d
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-01 13:11:55 +0400

    chap1exercise4.py

---
+++ Chapter 1- Getting Started with Python/exercise 4.py
@@ -0,0 +1,10 @@
+## Exercise 4: Strings Concatination 
+
+#Defining strings
+string1 = "Bath "
+string2 = "Spa "
+string3 = "University"
+
+#String Concatination
+school = string1+string2+string3
+print (school)
