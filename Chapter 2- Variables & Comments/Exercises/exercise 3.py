205add7b0880a1a504240dbeb1c65812101ff0d8
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-02 12:28:11 +0400

    chap2exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 3.py
@@ -0,0 +1,19 @@
+# Exercise 3: Stripping Names
+
+# Assigning a name to a variable
+print("Unmodified:")
+name="\t\n Miliani Leopoldo \n\t"
+print(name)
+
+# Print using lstrip()
+print("With .lstrip():")
+print (name.lstrip())
+
+# Print using rstrip()
+print("With .rstrip()")
+print (name.rstrip())
+
+# Print using strip()
+print("With .strip():")
+print (name.strip())
+
