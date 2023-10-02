fb95414545721d634b83da9b70cd284a63227333
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-02 12:27:58 +0400

    chap2exercise1.py

---
+++ Chapter 2- Variables & Comments/Exercises/exercise 2.py
@@ -0,0 +1,9 @@
+# Exercise 2: Variables 
+
+# Assigning author and quote
+author= "Taylor Swift"
+quote= "You're on your own now, but the thing is, you're on your own now!"
+
+
+# Printing Results
+print(f'{author} once said, "{quote}"')
