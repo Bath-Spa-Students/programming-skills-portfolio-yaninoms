8c41ebc14cbb7243c538fbdcc34952b80394d042
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-04 13:41:19 +0400

    practice4

---
+++ Chapter 1- Getting Started with Python/practice 4.py
@@ -0,0 +1,15 @@
+# Q4 Compute the Area of a triangle
+
+# Title
+print("Area of triangle")
+
+# Input of values  
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
+
+# Computation of Area
+s = (a + b + c) / 2
+
+# Results
+print(f"The Area of the triangle is {s}")
