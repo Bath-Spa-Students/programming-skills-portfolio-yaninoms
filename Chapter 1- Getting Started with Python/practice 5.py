9e1e555bad39c506d8c4e3c9a1b27ddf081dc0b8
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-04 13:41:43 +0400

    practice5

---
+++ Chapter 1- Getting Started with Python/practice 5.py
@@ -0,0 +1,10 @@
+# Write four strings in different variables and print the output as one string (string concatenation)
+
+# Assigning strings
+string1 = 'My'
+string2 = ' nname'
+string3 = ' is'
+string4 = ' Yani'
+
+string_con = string1+string2+string3+string4
+print(string_con)
