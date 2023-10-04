181df1396ef807e0a424a6d90db0aefcd2245fe0
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-04 13:41:04 +0400

    practice3

---
+++ Chapter 1- Getting Started with Python/practice 3.py
@@ -0,0 +1,7 @@
+# Q3: You need to make a program for a leaderboard. The program needs to output the numbers 1 to 9, each on a seperate line followed by a dot. 
+
+# Creating a range
+for num in range(1,10):
+   print(num,end='.\n')
+
+
