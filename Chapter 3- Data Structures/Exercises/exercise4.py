82bdb67af2348a907d515e8ac006a2bec2658508
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-05 13:47:28 +0400

    chap3exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,14 @@
+## Exercise 4: Guest List
+
+# Guest Names
+guests = ('mc', 'river', 'jhunnie')
+
+# Inviting the Guests Over
+name = guests[0].title()
+print(f"Hey {name}!, let's have a dinner at 8 PM.")
+
+name = guests[1].title()
+print(f"Hey {name}!, let's have a dinner at 8 PM.")
+
+name = guests[2].title()
+print(f"Hey {name}!, let's have a dinner at 8 PM.")
