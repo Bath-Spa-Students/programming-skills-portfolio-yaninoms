576248e98edd60a3e5c83cce0ae262a0a4d4147d
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-05 13:47:13 +0400

    chap3exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/exercise3.py
@@ -0,0 +1,9 @@
+# Exercise 3: Your Own List 
+
+#  List of favorite mode of transportation
+fav_transpo = ('public bus', 'metro', 'car')
+
+# Statements about the mode of transportation
+print(f"I used to commute for school through the AUH {fav_transpo[0]}.")
+print(f"I have barely experienced Dubai's {fav_transpo[1]}.")
+print(f"I want to own a Defender {fav_transpo[2]}.")
