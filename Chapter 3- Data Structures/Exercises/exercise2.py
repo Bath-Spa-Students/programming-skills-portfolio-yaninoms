baa42631f8205d645523f4de7fdd31872274a9b5
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-05 13:46:57 +0400

    chap3exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/exercise2.py
@@ -0,0 +1,9 @@
+## Exercise 2: Greetings 
+
+# The names
+names = ['Mc', 'Vernan','Jhunnie']
+
+# Printing the names with the same message
+print(f"How are you, {names[0]} ?")
+print(f"How are you, {names[1]} ?")
+print(f"How are you, {names[2]} ?")
