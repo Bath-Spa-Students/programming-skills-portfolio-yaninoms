efbb6c12cca494e5518f12662a2db48121fc917d
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-12 09:47:11 +0400

    practice.py

---
+++ Chapter 2- Variables & Comments/Exercises/practice.py
@@ -0,0 +1,16 @@
+
+
+import turtle
+turtle.forward(50)
+turtle.penup()
+turtle.forward(25)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.forward(50)
+turtle.penup()
+turtle.forward(25)
+turtle.penup()
+turtle.forward(50)
+turtle.pendown()
+turtle.done()
