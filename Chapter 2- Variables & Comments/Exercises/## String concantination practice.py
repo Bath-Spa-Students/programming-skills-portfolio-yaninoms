6b9a4da4bc22e06dc928a25b4cf84fb16edd2a4f
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-12 09:46:52 +0400

    practice

---
+++ Chapter 2- Variables & Comments/Exercises/## String concantination practice.py
@@ -0,0 +1,22 @@
+## String concantination practice
+
+first =(input('Enter first name: '))
+last = (input('Enter last name: '))
+
+print (' Welcome to Code Lab !')
+full_name = first + last
+print (full_name, end="\t")
+
+## Using the F-string
+
+print("## USING THE FSTRING\n")
+
+first =(input('Enter first name: '))
+last = (input('Enter last name: '))
+
+full_name = first + last
+print (f' Welcome to Code Lab ! "{full_name}"..')
+
+
+
+
