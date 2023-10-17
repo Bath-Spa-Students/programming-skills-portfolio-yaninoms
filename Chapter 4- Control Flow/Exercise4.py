1a83bfdef261312ca87e3a5841ee4aab121b1e06
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-17 10:26:42 +0400

    exercise4.py

---
+++ Chapter 4- Control Flow/Exercise4.py
@@ -0,0 +1,18 @@
+# Exercise 4
+
+age = 18
+
+if age < 2:
+    print("You're still a baby.")
+elif age < 4:
+    print("You're still a toddler.")
+elif age < 13:
+    print("You're still a tween.")
+elif age < 20:
+    print("You're still a teenager.")
+elif age > 65:
+    print("You're an adult!")
+else:
+    print("You're an elder.")
+
+
