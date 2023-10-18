bf4667c1358925fae8739e64d3fe10a45b0d902a
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-18 10:10:13 +0400

    exercise5.py

---
+++ Chapter 4- Control Flow/Exercise5.py
@@ -0,0 +1,15 @@
+# Exercise 5 : Favorite Fruits
+
+
+fav_fruits = ["Dragonfruit", "Banana", "Mango"]
+
+if 'Dragonfruit' in fav_fruits:
+    print("You really like Dragonfruits!")
+if 'Banana' in fav_fruits:
+    print ("You really like Bananas!")
+if 'Melon' in fav_fruits:
+    print("You really like Melons!")
+if 'Mandarins' in fav_fruits:
+    print("You really like Mandarins!")
+if 'Mango'  in fav_fruits:
+    print ("You really like Mangoes!")
