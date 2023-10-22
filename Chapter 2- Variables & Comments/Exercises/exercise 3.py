d50418bdca13c58ec72000961c5ccb8ecd684247
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-22 16:03:30 +0400

    chapter 2 exercises

--- Chapter 2- Variables & Comments/Exercises/exercise 3.py
+++ Chapter 2- Variables & Comments/Exercises/exercise 3.py
@@ -1,4 +1,9 @@
 # Exercise 3: Stripping Names
+'''Use a variable to represent a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, “\t” and “\n”, at least once.
+
+Print the name once, so the whitespace around the name is displayed.
+
+Then print the name using each of the three stripping functions, lstrip(), rstrip(), and strip().'''
 
 # Assigning a name to a variable
 print("Unmodified:")
