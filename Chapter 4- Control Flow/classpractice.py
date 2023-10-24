2ed2cc34ccc76af728f22d9ae54fe96b2d66331f
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-24 20:18:18 +0400

    chapter 4 class practice

--- Chapter 4- Control Flow/classpractice.py
+++ Chapter 4- Control Flow/classpractice.py
@@ -1,9 +1,33 @@
 # Class practice for exercise 4
 
-grade = float(input("Enter Final Grade:"))
+# Final Grade results
+
+# enter grade
+'''grade = float(input("Enter Final Grade:"))
+
+# result conditions
 result = "You have failed."
 if grade > 80:
     result = ("You have passed!")
 else:
     result =  ("You have failed.")
-print(f"\nFinal Grade: {grade},{result}")
+print(f"\nFinal Grade: {grade}\n{result}")'''
+
+
+# Visa approval
+
+# Enter requirements
+earning = int(input("\nEnter your earning per year:"))
+reason = str(input("Enter your reason of travel:"))
+
+# conditions
+if earning >= 40000: 
+    if reason == "business" or reason == "pleasure":
+        print("VISA APPROVED !")
+    else:
+       print("VISA DISAPPROVED: Sorry your reason of travel is not approved.")
+else:
+   print("VISA DISAPPROVED: Your earning is less than required.")
+
+
+
