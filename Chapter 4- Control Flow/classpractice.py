93ebcd6a995f508a673c2cfe89c9eb64b15c6e69
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-26 12:09:26 +0400

    chapter 4 practice

--- Chapter 4- Control Flow/classpractice.py
+++ Chapter 4- Control Flow/classpractice.py
@@ -11,7 +11,7 @@ if grade > 80:
     result = ("You have passed!")
 else:
     result =  ("You have failed.")
-print(f"\nFinal Grade: {grade}\n{result}")'''
+print(f"\nFinal Grade: {grade}\n{result}")
 
 
 # Visa approval
@@ -27,7 +27,47 @@ if earning >= 40000:
     else:
        print("VISA DISAPPROVED: Sorry your reason of travel is not approved.")
 else:
-   print("VISA DISAPPROVED: Your earning is less than required.")
+   print("VISA DISAPPROVED: Your earning is less than required.")'''
+
+
+   
+   
+print("Simple arithmetic calculator")
+print('''Addition : 1
+         Subtraction: 2
+         Multiplication: 3
+         Division: 4''')
+
+oper = input(print("Choose Operator:"))
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+if oper == "1":
+    result = num1 + num2
+    operation = "addition"
+elif oper == "2":
+    result = num1 - num2
+    operation = "subtraction"
+elif oper == "3":
+    result = num1*num2
+    operation = "multiplication"
+elif oper == "4":
+    if num2 == 0:
+        print("Invalid")
+    else:
+        result = num1 / num2
+        operation = "division"
+else:
+    if oper != "4":
+        print(f"The chosen operator is {oper} result: {result}.")
+    else:
+        print("Invalid")
+
+
+
+
+
+
 
 
 
