e967f3119aa88c3124acbe9db9f6dd7dd44c9952
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-10-31 10:32:32 +0400

    chapter 5 exercises

---
+++ Chapter 5- Dictionaries/Exercises/classpractice.py
@@ -0,0 +1,16 @@
+## Dictionary Practices
+dictionary = { 
+     'Name':'Miliani', 
+       'Age' : '17',
+       'Cat' : 'Nacci'}
+print(dictionary["Name"], type(dictionary))
+# ["Name"] u can check whether the item is in your dictionary
+
+print(dictionary.items())
+# prints the items in ur list
+
+print(dictionary.keys())
+# prints all the keys
+
+print(dictionary.values())
+# prints all the values
