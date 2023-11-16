a8b13312d2871776f41a3d6588fc1429791519bb
yaninoms <144207242+yaninoms@users.noreply.github.com>
2023-11-16 15:08:51 +0400

    chapter 7 practices

---
+++ Chapter 7- Functions/Exercises/practice1.py
@@ -0,0 +1,16 @@
+# Practice Question : Q1
+'''Write a Python function that takes two numbers as arguments and returns their sum'''
+
+# creating the function with 2 arguments
+def function(x,y):
+    sum = x + y # addition operation
+    return sum # return the sum
+
+# calling the function with given numbers
+result = function(68,49)
+print(result) # printing results
+
+
+
+
+
